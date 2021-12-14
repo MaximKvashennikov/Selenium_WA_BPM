aa8e1b910af0afe38789b4ba762b85759f2de46b
MaximKvashennikov <64595211+MaximKvashennikov@users.noreply.github.com>
2021-12-14 14:08:06 +0300

    update BPM

--- Report/get_report.py
+++ Report/get_report.py
@@ -15,7 +15,7 @@ class GetReport:
             "WFL_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%B8%D1%8F%20%D0%BD%D0%B0%20SW%20%D1%80%D0%B0%D1"
             "%81%D1%88%D0%B8%D1%80%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%A0%D0%A0%D0%9B")
         self.driver.maximize_window()
-        time.sleep(20)
+        time.sleep(22)
 
     def click_diskette(self):
         self.action.send_keys(Keys.TAB)
